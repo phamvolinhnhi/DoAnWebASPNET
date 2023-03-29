296e982d8c26171741579285d8a4e710d1459485
Nhi <phamvolinhnhi02@gmail.com>
2023-03-30 00:01:30 +0700

    add header layout

---
+++ WebSach/Scripts/myscripts/myjs.js
@@ -0,0 +1,9 @@
+﻿function show_bottom_menu(target) {
+    if ($(target).attr('class') == "fa fa-list") {
+        $(target).attr('class', 'fa fa-window-close');
+        $("#header_left_menu").attr('class', 'menu_mobile');
+    } else {
+        $(target).attr('class', 'fa fa-list');
+        $("#header_left_menu").attr('class', '');
+    }
+}

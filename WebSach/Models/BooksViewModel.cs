09fb1427c726bbb0138674dd68f9deb3aa8917bf
Nhi <phamvolinhnhi02@gmail.com>
2023-04-05 04:27:56 +0700

    edit details

---
+++ WebSach/Models/BooksViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSach.Models
+{
+    public class BooksViewModel
+    {
+        public Books books;
+        public List<Chapter> chapters;
+        public List<Comment> comments;
+    }
+}

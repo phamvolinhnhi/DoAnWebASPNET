b2ddbd7306e0885373fa9b146c433baf24c5f252
Nhi <phamvolinhnhi02@gmail.com>
2023-04-05 04:37:48 +0700

    edit details

--- WebSach/Controllers/BooksController.cs
+++ WebSach/Controllers/BooksController.cs
@@ -33,6 +33,7 @@ namespace WebSach.Controllers
                 books = FindBookById(id.Value),
                 Chapters = _db.Chapter.Where(c => c.Book_Id == id.Value).ToList(),
                 Comments = _db.Comment.Where(c => c.Book_Id == id.Value).ToList(),
+
             };
             return View(booksviewmodel);
         }
@@ -48,6 +49,7 @@ namespace WebSach.Controllers
         {
             return _db.Books.FirstOrDefault(p => p.Book_Id == id);
         }
+
         public ActionResult Chapter(int? bookid, int? id)
         {
             if (id == null || bookid == null)
@@ -55,7 +57,6 @@ namespace WebSach.Controllers
                 return HttpNotFound();
             }
             Chapter chapter = _db.Chapter.Where(c => c.Chapter_Id == id.Value && c.Book_Id == bookid).FirstOrDefault();
-
             if (chapter == null)
             {
                 return HttpNotFound();
